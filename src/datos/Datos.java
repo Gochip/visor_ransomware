42f16b88dfa40ebfd78ea41070d76d8097ad1361
Germán Parisi <germannparisi@gmail.com>
2018-05-12 01:56:08 -0300

    Agrego nuevo ransomware descagar de GitHub y adaptado

--- src/datos/Datos.java
+++ src/datos/Datos.java
@@ -9,7 +9,9 @@ public class Datos {
     public static ArrayList<Ransomware> cargarRansomwares() {
         ransomwares = new ArrayList<>();
         Ransomware r1 = new RansomwareJAES128("JAES-128", "Ransomware de cifrado simétrico AES - 128 bits para análisis académico. Parámetros: clave=1234567812345678");
+        Ransomware r2 = new Jamsomware("Jamsomware", "Ransomware adaptado y descargado desde https://github.com/julupu/jamsomware");
         ransomwares.add(r1);
+        ransomwares.add(r2);
         return ransomwares;
     }
 }
